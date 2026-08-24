17b659620bfc14899d938c4bd5f98c8b9d58e6fb
Alexey <alexexmypride@gmail.com>
2026-08-24 15:10:12 +0300

    added `SECURE_PROXY_SSL_HEADER`

--- config/settings.py
+++ config/settings.py
@@ -15,6 +15,7 @@ SECRET_KEY = env("SECRET_KEY")
 
 DEBUG = env.bool('DEBUG', default=False)
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost'])
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
 INSTALLED_APPS = [
